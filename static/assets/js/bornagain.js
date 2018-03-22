d0310861a479910d89a7e2f237a6e8747275a269
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-03-22 15:00:40 +0100

    Switched to common js to get OS info

--- static/assets/js/bornagain.js
+++ static/assets/js/bornagain.js
@@ -1,22 +1,16 @@
 jQuery(document).ready(function($) {
-  // operation system detection
-  var OSName = "Unknown OS";
-  if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
-  else if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
-  else if (navigator.appVersion.indexOf("X11") != -1) OSName = "Linux";
-  else if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
-
   // Additional activation of first tab in tabs-nav.md example 
   // Activiation is necessary because of fade effect
   $('#myTab2 li:first-child a').tab('show')
 
   // Shows tab corresponding to operation system (thirs tab in tabs-nav.md example)
+  var OSName = osName();
   if (OSName == "Windows") {
-    $('#myTab3 li a[href="#Windows"]').tab('show') 
+    $('#DonwloadTab li a[href="#Windows"]').tab('show') 
   } else if(OSName == "MacOS") {
-    $('#myTab3 li a[href="#MacOS"]').tab('show') 
+    $('#DonwloadTab li a[href="#MacOS"]').tab('show') 
   } else if(OSName == "Linux") {
-    $('#myTab3 li a[href="#Linux"]').tab('show') 
+    $('#DonwloadTab li a[href="#Linux"]').tab('show') 
   }
 
 });
