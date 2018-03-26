9532786513c3c31090694b385278fc072dc60cc4
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-03-26 14:09:18 +0200

    Unused js code removed

--- static/assets/js/bornagain.js
+++ static/assets/js/bornagain.js
@@ -1,8 +1,4 @@
 jQuery(document).ready(function($) {
-  // Additional activation of first tab in tabs-nav.md example 
-  // Activiation is necessary because of fade effect
-  $('#myTab2 li:first-child a').tab('show')
-
   // Shows tab corresponding to operation system (thirs tab in tabs-nav.md example)
   var OSName = osName();
   if (OSName == "Windows") {
