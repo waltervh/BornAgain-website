a24136b8ae039c13fba57221b191f3a3c6b6c4d9
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-03-26 14:12:34 +0200

    Tab id changed for reuse

--- static/assets/js/bornagain.js
+++ static/assets/js/bornagain.js
@@ -2,11 +2,11 @@ jQuery(document).ready(function($) {
   // Shows tab corresponding to operation system (thirs tab in tabs-nav.md example)
   var OSName = osName();
   if (OSName == "Windows") {
-    $('#DownloadTab li a[href="#Windows"]').tab('show') 
+    $('#OperationSystemTab li a[href="#Windows"]').tab('show') 
   } else if(OSName == "MacOS") {
-    $('#DownloadTab li a[href="#MacOS"]').tab('show') 
+    $('#OperationSystemTab li a[href="#MacOS"]').tab('show') 
   } else if(OSName == "Linux") {
-    $('#DownloadTab li a[href="#Linux"]').tab('show') 
+    $('#OperationSystemTab li a[href="#Linux"]').tab('show') 
   }
 
 });
