a589ea3b08587596ecff02b886a0140584b587b4
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2019-07-12 15:39:29 +0200

    Update DepthProbe.py

--- content/documentation/working-with-python/basic-simulation-tutorial/depth-probe/DepthProbe.py
+++ content/documentation/working-with-python/basic-simulation-tutorial/depth-probe/DepthProbe.py
@@ -1,13 +1,58 @@
 """
 Basic example of depth-probe simulation with BornAgain.
+Sample structure:
+----------------------- inf
+
+                        Si
+
+----------------------- 0 nm
+                        Ti
+----------------------- -13 nm
+                        Pt
+----------------------- -45 nm
+                        Ti
+----------------------- -55 nm
+                        TiO2
+----------------------- -58 nm
+
+                        D2O
+
+----------------------- -inf
+Beam comes from silicon side.
+z axis is directed up and perpendicularly
+to the sample, z = 0 corresponding to the sample
+surface
 """
 import bornagain as ba
 from bornagain import deg, angstrom, nm
 
+# layer thicknesses in angstroms
+t_Ti = 130.0 * angstrom
+t_Pt = 320.0 * angstrom
+t_Ti_top = 100.0 * angstrom
+t_TiO2 = 30.0 * angstrom
+
+#  beam data
+ai_min = 0.0 * deg  # minimum incident angle
+ai_max = 1.0 * deg  # maximum incident angle
+n_ai_bins = 5000    # number of bins in incident angle axis
+beam_sample_ratio = 0.01  # beam-to-sample size ratio
+wl = 10 * angstrom  # wavelength in angstroms
+
+# convolution parameters
+d_ang = 0.01 * ba.deg  # spread width for incident angle
+n_points = 25  # number of points to convolve over
+n_sig = 3  # number of sigmas to convolve over
+
+#  depth position span
+z_min = -100 * nm
+z_max = 100 * nm
+n_z_bins = 500
+
 
 def get_sample():
     """
-    Constructs a sample with one resonating Ti/Pt bilayer
+    Constructs a sample with one resonating Ti/Pt layer
     """
 
     # define materials
@@ -25,7 +70,7 @@ def get_sample():
     l_TiO2 = ba.Layer(m_TiO2, 30.0 * angstrom)
     l_D2O = ba.Layer(m_D2O)
 
-    # construct sample from top to bottom
+    # construct sample
     sample = ba.MultiLayer()
     sample.addLayer(l_Si)
 
@@ -43,9 +88,13 @@ def get_simulation():
     """
     Returns a depth-probe simulation.
     """
+    alpha_distr = ba.DistributionGaussian(0.0, d_ang)
+    footprint = ba.FootprintFactorSquare(beam_sample_ratio)
     simulation = ba.DepthProbeSimulation()
-    simulation.setBeamParameters(10 * angstrom, 5000, 0.0 * deg, 1.0 * deg)
-    simulation.setZSpan(500, -100 * nm, 100 * nm)
+    simulation.setBeamParameters(wl, n_ai_bins, ai_min, ai_max, footprint)
+    simulation.setZSpan(n_z_bins, z_min, z_max)
+    simulation.addParameterDistribution("*/Beam/InclinationAngle", alpha_distr,
+                                        n_points, n_sig)
     return simulation
 
 
@@ -62,4 +111,4 @@ def run_simulation():
 
 if __name__ == '__main__':
     result = run_simulation()
-    ba.plot_simulation_result(result)
+    ba.plot_simulation_result(result, cmap='jet', aspect='auto')
